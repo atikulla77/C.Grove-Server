99cbf26b8c6c95a24f58cd20ca35f93662704f6b
Kodify <96272995+atikulla77@users.noreply.github.com>
2026-01-06 04:03:33 +0600

    updated

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,8 @@
-import serverless from "serverless-http";
-import app from "../src/app"; // your Express app
+// import serverless from "serverless-http";
+// import app from "../src/app"; // your Express app
 
-export const handler = serverless(app);
+// export const handler = serverless(app);
+
+export default function handler(req: any, res: any) {
+  res.status(200).json({ message: "Serverless function works!" });
+}
