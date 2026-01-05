f054612715902c55935a66e82cc606eb68fa5afc
Kodify <96272995+atikulla77@users.noreply.github.com>
2026-01-06 04:28:18 +0600

    updated

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,23 @@
 import serverless from "serverless-http";
-import app from "../src/app"; // your Express app
+import app from "../src/app";
 
-export default serverless(app);
+const handler = serverless(app);
+
+export default async (req: any, res: any) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://cgrove.vercel.app");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,OPTIONS"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  return handler(req, res);
+};
